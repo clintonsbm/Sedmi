5eb5fcf77b6ee52710a270caa0e66234a68d345a
Clinton de Sá Barreto Maciel <clintonsbm@gmail.com>
2018-04-25 21:42:48 -0300

    Initial Commit

---
+++ Sedmi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sedmi
+//
+//  Created by Clinton de Sá Barreto Maciel on 25/04/18.
+//  Copyright © 2018 Clinton de Sá. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SedmiTests/SedmiTests.swift
@@ -0,0 +1,36 @@
+//
+//  SedmiTests.swift
+//  SedmiTests
+//
+//  Created by Clinton de Sá Barreto Maciel on 25/04/18.
+//  Copyright © 2018 Clinton de Sá. All rights reserved.
+//
+
+import XCTest
+@testable import Sedmi
+
+class SedmiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
